6be060660843c521d048e1649b3becd0e9939859
Nishant Singh <72272867+vasu101@users.noreply.github.com>
2025-01-10 14:19:32 +0530

    Profile-Card
    1st draft of the Profile-Card project
    Issues to resolve in next session
    -> Align the profile pic properly
    -> Change the background with better colours
    -> consider adding more details in card

--- Day1/Profile-card/src/App.jsx
+++ Day1/Profile-card/src/App.jsx
@@ -1,9 +1,21 @@
+import ProfileCard from "./Components/ProfileCard"
+import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+      <div className="background">
+			<div className="bg-pattern-top"></div>
+			<div className="bg-pattern-bottom"></div>
+
+			<ProfileCard
+				name="Nishant"
+				age="24"
+				city="Jaipur, Raj"
+				followers="128"
+				likes="25K"
+				photos="21"
+			></ProfileCard>
+		</div>
   )
 }
 
